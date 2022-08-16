079e0d16f4f156bb43af94adc43473c5c4c6ffcc
Patrick Bradshaw <patrick.j.bradshaw@gmail.com>
2022-08-16 01:21:35 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import './assets/styles/App.css'
 import CategoryTable from './features/recipe-data/components/CategoryTable'
 import IngredientsTable from './features/recipe-data/components/IngredientsTable'
 import { useRecipeData } from './features/recipe-data/hooks'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
    const recipeData = useRecipeData()
+   const [date, setDate] = React.useState<any>((new Date()).toISOString().substring(0, 10))
 
    console.log(recipeData)
+   console.log(date)
 
    return (
       <div className="App">
          {/* <IngredientsTable /> */}
-         <CategoryTable />
+         {/* <CategoryTable /> */}
+         <input type="date" value={date} onChange={e => setDate(e.currentTarget.value)} />
       </div>
    )
 }
